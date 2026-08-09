882f2fe7ed8f40de3cbd84f41807ec71f7cf42db
Haris <haris_are@outlook.com>
2026-08-09 11:10:24 +0700

    feat: update usePageTitle hook to include i18n support for dynamic document titles

--- src/hooks/usePageTitle.tsx
+++ src/hooks/usePageTitle.tsx
@@ -1,9 +1,15 @@
 import { useEffect } from "react";
 
+// i18n
+import { useTranslation } from "react-i18next";
+
 const usePageTitle = (title: string) => {
+  // i18n
+  const { t } = useTranslation();
+
   useEffect(() => {
-    document.title = `${title} | NSB User Management`;
-  }, [title]);
+    document.title = title ? `${title} | ${t('project.title')}` : t('project.title');
+  }, [title, t]);
 };
 
 export default usePageTitle;
